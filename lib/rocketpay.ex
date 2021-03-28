d97b3b8f5b0c3896462827b4de3d920eaf60a37b
Antônio Sérgio Jr <sergio.gxp@gmail.com>
2021-03-28 17:23:50 -0300

    Criação transaction - Aula 4

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,11 +1,11 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
 
-  alias Rocketpay.Accounts.{Deposit}
+  alias Rocketpay.Accounts.{Deposit, Withdraw, Transaction}
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit_value(params), to: Deposit, as: :call
-  #defdelegate withdraw_value(params), to: Withdraw, as: :call
-  #defdelegate transaction(params), to: Transaction, as: :call
+  defdelegate withdraw_value(params), to: Withdraw, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
 end
